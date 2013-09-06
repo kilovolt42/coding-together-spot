e5b6c060057e453c5b8e17c1a7ce1c7e2ccc07ca
kilovolt42 <kvstevns@gmail.com>
2013-09-06 13:04:50 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# SPoT
+
+This is my work on the SPoT project from Stanford University's [Coding Together](https://itunes.apple.com/us/course/coding-together-developing/id593208016) course.
---
+++ SPoT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPoT
+//
+//  Created by Kyle Stevens on 9/6/13.
+//  Copyright (c) 2013 Kyle Stevens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
