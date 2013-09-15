eb29ab362b4ca8645f6ae16e994846277281d0a7
kilovolt42 <kvstevns@gmail.com>
2013-09-15 13:36:44 -0400

    (Assignment 5 Tasks 4 and 5) Caches downloaded images.

--- SPoT/ImageScrollViewController.m
+++ SPoT/ImageScrollViewController.m
@@ -50,28 +50,37 @@
 		self.scrollView.contentSize = CGSizeZero;
 		self.imageView.image = nil;
 		
-		[self.activityIndicator startAnimating];
-		NSURL *imageURL = self.imageURL;
-		dispatch_queue_t downloadQueue = dispatch_queue_create("image download queue", NULL);
-		dispatch_async(downloadQueue, ^{
-			[IndicatorActivator activate];
-			NSData *imageData = [[NSData alloc] initWithContentsOfURL:self.imageURL];
-			[IndicatorActivator deactivate];
-			UIImage *image = [[UIImage alloc] initWithData:imageData];
-			if (self.imageURL == imageURL) {
-				dispatch_async(dispatch_get_main_queue(), ^{
-					[self.activityIndicator stopAnimating];
-					if (image) {
-						self.scrollView.zoomScale = 1.0;
-						self.scrollView.contentSize = image.size;
-						self.imageView.image = image;
-						self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
-						self.userZoomed = NO;
-						[self autoZoom];
-					}
-				});
-			}
-		});
+		if (self.imageURL) {
+			[self.activityIndicator startAnimating];
+			NSURL *imageURL = self.imageURL;
+			dispatch_queue_t loadingQueue = dispatch_queue_create("image loading queue", NULL);
+			dispatch_async(loadingQueue, ^{
+				NSData *imageData = [self cachedDataForURL:self.imageURL];
+				if (!imageData) {
+					[IndicatorActivator activate];
+					imageData = [[NSData alloc] initWithContentsOfURL:self.imageURL];
+					[IndicatorActivator deactivate];
+					dispatch_queue_t cachingQueue = dispatch_queue_create("image caching queue", NULL);
+					dispatch_async(cachingQueue, ^{
+						[self cacheData:imageData forURL:self.imageURL];
+					});
+				}
+				UIImage *image = [[UIImage alloc] initWithData:imageData];
+				if (self.imageURL == imageURL) {
+					dispatch_async(dispatch_get_main_queue(), ^{
+						[self.activityIndicator stopAnimating];
+						if (image) {
+							self.scrollView.zoomScale = 1.0;
+							self.scrollView.contentSize = image.size;
+							self.imageView.image = image;
+							self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+							self.userZoomed = NO;
+							[self autoZoom];
+						}
+					});
+				}
+			});
+		}
 	}
 }
 
@@ -116,6 +125,68 @@
 - (void)viewDidLayoutSubviews {
 	[super viewDidLayoutSubviews];
 	[self autoZoom];
+}
+
+#pragma mark - Image data caching
+
+#define CACHE_SUBDIRECTORY @"SPoT_Photos_Cache"
+#define CACHE_SIZE_LIMIT_PHONE 3000000
+#define CACHE_SIZE_LIMIT_PAD 8000000
+
+- (NSData *)cachedDataForURL:(NSURL *)url {
+	NSFileManager *fileManager = [[NSFileManager alloc] init];
+	NSURL *directory = [fileManager URLsForDirectory:NSCachesDirectory inDomains:NSAllDomainsMask][0];
+	directory = [directory URLByAppendingPathComponent:CACHE_SUBDIRECTORY];
+	NSURL *fileURL = [directory URLByAppendingPathComponent:[url lastPathComponent]];
+	return [fileManager contentsAtPath:[fileURL path]];
+}
+
+- (void)cacheData:(NSData *)data forURL:(NSURL *)url {
+	NSFileManager *fileManager = [[NSFileManager alloc] init];
+	NSURL *directory = [fileManager URLsForDirectory:NSCachesDirectory inDomains:NSAllDomainsMask][0];
+	directory = [directory URLByAppendingPathComponent:CACHE_SUBDIRECTORY];
+	
+	NSError *error;
+	[fileManager createDirectoryAtPath:[directory path] withIntermediateDirectories:NO attributes:nil error:&error];
+	if (error && [error code] != 516) NSLog(@"Error: %@", error);
+	
+	NSURL *fileURL = [directory URLByAppendingPathComponent:[url lastPathComponent]];
+	[fileManager createFileAtPath:[fileURL path] contents:data attributes:nil];
+	
+	NSArray *cachedPhotos = [fileManager contentsOfDirectoryAtURL:directory
+									   includingPropertiesForKeys:@[NSURLContentAccessDateKey, NSURLFileSizeKey]
+														  options:NSDirectoryEnumerationSkipsHiddenFiles
+															error:&error];
+	if (!cachedPhotos) NSLog(@"Error: %@", error);
+	
+	NSMutableArray *sortedCachedPhotos = [[cachedPhotos sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+		NSDate *photo1Date;
+		NSDate *photo2Date;
+		NSError *error;
+		if (![(NSURL *)obj1 getResourceValue:&photo1Date forKey:NSURLContentAccessDateKey error:&error]) NSLog(@"Error: %@", error);
+		if (![(NSURL *)obj2 getResourceValue:&photo2Date forKey:NSURLContentAccessDateKey error:&error]) NSLog(@"Error: %@", error);
+		return [photo1Date compare:photo2Date];
+	}] mutableCopy];
+	
+	NSUInteger cacheSizeLimit;
+	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) cacheSizeLimit = CACHE_SIZE_LIMIT_PHONE;
+	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) cacheSizeLimit = CACHE_SIZE_LIMIT_PAD;
+	while ([self fileSizeSumForURLs:sortedCachedPhotos] > cacheSizeLimit) {
+		NSURL *oldest = sortedCachedPhotos[0];
+		if (![fileManager removeItemAtURL:oldest error:&error]) NSLog(@"Error: %@", error);
+		[sortedCachedPhotos removeObjectAtIndex:0];
+	}
+}
+
+- (NSUInteger)fileSizeSumForURLs:(NSArray *)urls {
+	NSUInteger sum = 0;
+	NSNumber *size;
+	NSError *error;
+	for (NSURL *url in urls) {
+		if (![url getResourceValue:&size forKey:NSURLFileSizeKey error:&error]) NSLog(@"Error: %@", error);
+		sum += [size integerValue];
+	}
+	return sum;
 }
 
 @end
